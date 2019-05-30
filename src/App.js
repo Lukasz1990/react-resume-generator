4c8492af539818724dd0a68627cbddc62a603ccf
Lukasz1990 <lukasz.fijalkowski1337@gmail.com>
2019-05-31 00:54:59 +0200

    some api request change

--- src/App.js
+++ src/App.js
@@ -1,56 +1,50 @@
 import React from 'react';
 import axios from 'axios';
+import './App.css';
 
-const client_id = "Iv1.0e5b90fc3244118b";
-const client_secret = "c957b1bdc5db654a4d68cdd11d70b3f3a921b8aa";
 
 class App extends React.Component {
   state = {
     users: [],
-    loaded: false,
+    error: false,
   }
-  
+
 
   componentDidMount() {
-    const fetchdata = async (user)=>{
-      return axios.get(`https://api.github.com/users/${user}/repos?client_id=${client_id}&client_secret=${client_secret}`)
+    const fetchdata =  async (user)=>{
+      return axios.get(`https://api.github.com/users/${user}/repos`)
       .then(res=>{
-     return res.data
+return res.data;
       }).then (data=>{
-         data.map(project=>{
-          return axios.get(`${project.languages_url}?client_id=${client_id}&client_secret=${client_secret}`)
-          .then(res=>{
-            return res.data
-          }).then(repos=>{
-          
-           console.log(repos)
-            const keys = Object.keys(repos)
-            // const entries = Object.entries(repos)
-            console.log(...keys)
-            // console.log(...entries)
-            //tutaj się pogubiłem, wydaje mi się, że trzeba by było jakoś przekonwertować te objekty w tablice a potem porównać keys
-        
-
-
-         
-
-          })
-          
+        console.log(data)
+         return data.map(project=>{
+         return project.name
+        })    
+       }).then(res=>{
+        res.forEach(name => {
+       
+                  axios.get (`https://api.github.com/repos/Lukasz1990/${name}/languages`).then(res=>{
+                
+                    return res.data                            
         })
-        
-
-     
-
       })
-    }
+    }).then(res=>{
+      console.log(res)
+    })
+  }
+
+    
     fetchdata('Lukasz1990') //w parametrze będzie target.value do inputa, to tylko tymczasowo :
 
-  }
-  
+}
+
+
 
 render() 
+
 {
   return (
+    
     <div className="App">
     <ul>
 <li>
@@ -60,9 +54,10 @@ render()
     </div>
   );
 }
-
-
 }
+
+
+
 
 
 
